6f21f1f5d7be5fe01db6d6ba46c6ef572dbbe51a
Nestor Arocha <nesaro@gmail.com>
2015-06-11 14:33:51 +0100

    adds Opportunity class

--- capsule_api/capsule_api.py
+++ capsule_api/capsule_api.py
@@ -1,20 +1,31 @@
 import requests
+import dateutil.parser
 
-CAPSULE_NAME = ''
-BASE_URL = "https://%s.capsulecrm.com/api/" % CAPSULE_NAME
-CAPSULE_KEY = ''
+class Opportunity(dict):
+    @property
+    def createdOn(self):
+        return dateutil.parser.parse(self['createdOn'])
 
+    @property
+    def expectedCloseDate(self):
+        return dateutil.parser.parse(self['expectedCloseDate'])
+
+    @property
+    def open(self):
+        return 'actualCloseDate' not in self
 
 class CapsuleAPI(object):
-    def __init__(self):
-        self.base_url = BASE_URL 
+    def __init__(self, capsule_name, capsule_key):
+        self.capsule_name = capsule_name
+        self.capsule_key = capsule_key
+        self.base_url = "https://%s.capsulecrm.com/api/" % capsule_name
 
     def request(self, method, path, **kwargs):
         headers = {
             'accept': 'application/json',
             'content-type': 'application/json' 
         }
-        auth = requests.auth.HTTPBasicAuth(CAPSULE_KEY, CAPSULE_NAME)
+        auth = requests.auth.HTTPBasicAuth(self.capsule_key, self.capsule_name)
         if method.lower() == 'get':
             return requests.get(self.base_url + path , headers=headers, params=kwargs, auth=auth).json()
         else:
@@ -27,10 +38,12 @@ class CapsuleAPI(object):
         get_options = ''
         if kwargs:
             get_options = '?' + "&".join([x + "=" + y for (x,y) in kwargs.items()])
-        return self.get('opportunity' + get_options)
+        result = self.get('opportunity' + get_options)
+        result['opportunities']['opportunity'] = [Opportunity(x) for x in result['opportunities']['opportunity']]
+        return result
 
 if __name__ == '__main__':
-    crm = CapsuleAPI()
-    print(crm.opportunities(status="open"))
+    crm = CapsuleAPI("name", "key")
+    print(crm.opportunities())
     
 
